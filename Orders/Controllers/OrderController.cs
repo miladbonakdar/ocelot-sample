1f19b8302c18a398c149e92431695136cde4abb3
milad <miladbonak@gmail.com>
2020-03-02 21:53:41 +0330

    api created

---
+++ Orders/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Orders.Controllers
+{
+    [ApiController]
+    [Route("Order")]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Order> Get()
+        {
+            return new[]
+            {
+                new Order(2345,12413),
+                new Order(765432,1241456),
+            };
+        }
+    }
+    
+    public class Order
+    {
+        public Order(long id, decimal finalPrice)
+        {
+            FinalPrice = finalPrice;
+            Id = id;
+        }
+
+        public decimal FinalPrice { get; }
+        public long Id { get; }
+    }
+}
---
+++ Users/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Users.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return new[]
+            {
+                new User("milad", "bonakdar"),
+                new User("omid", "naghipoor"),
+                new User("rasoool", "ghana")
+            };
+        }
+    }
+
+    public class User
+    {
+        public User(string name, string lastname)
+        {
+            Name = name;
+            Lastname = lastname;
+        }
+
+        public string Name { get; }
+        public string Lastname { get; }
+    }
+}
