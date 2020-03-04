8e21c9b7a320e820dc6b4babdb8c4a927f2cc374
milad <miladbonak@gmail.com>
2020-03-04 13:55:55 +0330

    some enhancements

---
+++ Gateway/Aggregator/UserAndOrderCustomAggregator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Ocelot.Middleware;
+using Ocelot.Middleware.Multiplexer;
+
+namespace Gateway.Aggregator
+{
+    public class UserAndOrderCustomAggregator : IDefinedAggregator
+    {
+        public async Task<DownstreamResponse> Aggregate(List<DownstreamContext> responses)
+        {
+            var userTask = responses.First(r => r.DownstreamReRoute.Key == "User").DownstreamResponse.Content
+                .ReadAsStringAsync();
+            var orderTask = responses.First(r => r.DownstreamReRoute.Key == "Order").DownstreamResponse.Content
+                .ReadAsStringAsync();
+
+            var res = await Task.WhenAll(userTask, orderTask);
+            var user = res[0];
+            var order = res[1];
+
+            var contentBuilder = new StringBuilder();
+            contentBuilder.Append("{\"User\":");
+            contentBuilder.Append(user);
+            contentBuilder.Append(",\"Order\":");
+            contentBuilder.Append(order);
+            contentBuilder.Append("}");
+
+            var stringContent = new StringContent(contentBuilder.ToString())
+            {
+                Headers = {ContentType = new MediaTypeHeaderValue("application/json")}
+            };
+
+            return new DownstreamResponse(stringContent, HttpStatusCode.OK,
+                new List<KeyValuePair<string, IEnumerable<string>>>()
+                , "OK");
+        }
+    }
+}
--- Gateway/Startup.cs
+++ Gateway/Startup.cs
@@ -1,18 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
+using Gateway.Aggregator;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using Ocelot.Middleware.Multiplexer;
 
 namespace Gateway
 {
@@ -40,37 +32,6 @@ namespace Gateway
             app.UseAuthorization();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
             await app.UseOcelot();
-        }
-    }
-
-    public class UserAndOrderCustomAggregator : IDefinedAggregator
-    {
-        public async Task<DownstreamResponse> Aggregate(List<DownstreamContext> responses)
-        {
-            var userTask = responses.First(r => r.DownstreamReRoute.Key == "User").DownstreamResponse.Content
-                .ReadAsStringAsync();
-            var orderTask = responses.First(r => r.DownstreamReRoute.Key == "Order").DownstreamResponse.Content
-                .ReadAsStringAsync();
-
-            var res = await Task.WhenAll(userTask, orderTask);
-            var user = res[0];
-            var order = res[1];
-
-            var contentBuilder = new StringBuilder();
-            contentBuilder.Append("{\"User\":");
-            contentBuilder.Append(user);
-            contentBuilder.Append(",\"Order\":");
-            contentBuilder.Append(order);
-            contentBuilder.Append("}");
-
-            var stringContent = new StringContent(contentBuilder.ToString())
-            {
-                Headers = {ContentType = new MediaTypeHeaderValue("application/json")}
-            };
-
-            return new DownstreamResponse(stringContent, HttpStatusCode.OK,
-                new List<KeyValuePair<string, IEnumerable<string>>>()
-                , "OK");
         }
     }
 }
