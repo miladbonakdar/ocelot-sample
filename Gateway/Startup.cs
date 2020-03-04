cda5b706228156d94e2f921d3b701e698c752e39
milad <miladbonak@gmail.com>
2020-03-04 13:52:45 +0330

    some aggrigations added

--- Gateway/Startup.cs
+++ Gateway/Startup.cs
@@ -1,11 +1,18 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Middleware.Multiplexer;
 
 namespace Gateway
 {
@@ -22,7 +29,8 @@ namespace Gateway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddOcelot(Configuration);
+            services.AddOcelot(Configuration)
+                .AddSingletonDefinedAggregator<UserAndOrderCustomAggregator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +40,37 @@ namespace Gateway
             app.UseAuthorization();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
             await app.UseOcelot();
+        }
+    }
+
+    public class UserAndOrderCustomAggregator : IDefinedAggregator
+    {
+        public async Task<DownstreamResponse> Aggregate(List<DownstreamContext> responses)
+        {
+            var userTask = responses.First(r => r.DownstreamReRoute.Key == "User").DownstreamResponse.Content
+                .ReadAsStringAsync();
+            var orderTask = responses.First(r => r.DownstreamReRoute.Key == "Order").DownstreamResponse.Content
+                .ReadAsStringAsync();
+
+            var res = await Task.WhenAll(userTask, orderTask);
+            var user = res[0];
+            var order = res[1];
+
+            var contentBuilder = new StringBuilder();
+            contentBuilder.Append("{\"User\":");
+            contentBuilder.Append(user);
+            contentBuilder.Append(",\"Order\":");
+            contentBuilder.Append(order);
+            contentBuilder.Append("}");
+
+            var stringContent = new StringContent(contentBuilder.ToString())
+            {
+                Headers = {ContentType = new MediaTypeHeaderValue("application/json")}
+            };
+
+            return new DownstreamResponse(stringContent, HttpStatusCode.OK,
+                new List<KeyValuePair<string, IEnumerable<string>>>()
+                , "OK");
         }
     }
 }
--- Orders/Controllers/OrderController.cs
+++ Orders/Controllers/OrderController.cs
@@ -16,12 +16,18 @@ namespace Orders.Controllers
         {
             return new[]
             {
-                new Order(2345,12413),
-                new Order(765432,1241456),
+                new Order(2345, 12413),
+                new Order(765432, 1241456),
             };
         }
+
+        [HttpGet("{Id}")]
+        public Order Get(int id)
+        {
+            return new Order(id, 678);
+        }
     }
-    
+
     public class Order
     {
         public Order(long id, decimal finalPrice)
